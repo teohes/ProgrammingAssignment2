2cfcafc369b69655912034a76bce033d261feeab
teohes <teohes@vomfassmy.com>
2015-02-17 01:23:41 +0800

    Further comments added wrt <<-

--- cachematrix.R
+++ cachematrix.R
@@ -14,35 +14,36 @@
 ## The critical/definitive operator to achieve this is by use of the "Super-assignment" 
 ## operator  " <<- ".
 ## NOTE:  makeCacheMatrix will only work on "invertible" matrices eg.  SQUARE matrix.
+## NOTE:  makeCacheMatrix will not detect non-invertible matrices (ie.  no checking is done)
 
 makeCacheMatrix <- function(x = matrix()) {
-        mInv <- NULL
+        mInv <- NULL                                # mInv= matrix inverse
         set <- function(y) {
-                x <<- y
-                mInv <<- NULL
-        }
+                x <<- y                             # NOTE:  the <<- looks outside the current scope
+				mInv <<- NULL                       # starting with the parents environment...
+  		}											# (like global/common space ?)                       
         get <- function() x
         setInv <- function(solve) mInv <<- solve
         getInv <- function() mInv
-        list(set = set, get = get, setInv = setInv,
-             getInv = getInv)
-}
+        list(set = set, get = get, setInv = setInv,   # format:  set1=set2 where set1=list element name
+             getInv = getInv)                         # and set2=function set2().  Same goes for get=get etc..
+}                                                     # this is how cacheSolve can get to these functions in 
+                                                      # makeCacheMatrix
 
-
-## Given a matrix "x" that was created using "makeCacheMatrix" above, cacheSolve will
-## resort to using the previously calculated (cached) value of the inverse of x.  If the 
-## inverse of x does not exist, it will calculate it using solve() the first time and cache
+## cacheSolve: Given a matrix "x" that was created using "makeCacheMatrix" above, cacheSolve will
+## resort to using the previously computed (cached) value of the inverse of x.  If the 
+## inverse of x does not exist, it will compute it using solve() the first time and cache
 ## the results for future use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-	    mInv <- x$getInv()
-        if(!is.null(mInv)) {
+	    mInv <- x$getInv()                       # mInv gets loaded with NULL initally by makeCacheMatrix 
+        if(!is.null(mInv)) {                     # so if not NULL, just return the cached mInv
                 message("getting cached data")
                 return(mInv)
         }
-        data <- x$get()
-        mInv <- solve(data, ...)
-        x$setInv(mInv)
-        mInv
+        data <- x$get()                          # otherwise get the original matrix 
+        mInv <- solve(data, ...)                 # compute the inverse and 
+        x$setInv(mInv)                           # cache it
+        mInv                                     # don't forget to return the computed inverse
 }
